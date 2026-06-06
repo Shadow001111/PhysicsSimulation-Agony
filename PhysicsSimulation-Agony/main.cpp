d87debbc763dd60f2666e9fc5810032571c0f90f
ShadowBlader <sutuginaleksandr1@gmail.com>
2026-06-06 23:29:47 +0300

    Galton board.

--- PhysicsSimulation-Agony/main.cpp
+++ PhysicsSimulation-Agony/main.cpp
@@ -117,6 +117,144 @@ static void renderDebugText(float aspectRatio, const DebugData& debugData)
     TextRenderer::renderText(text, -aspectRatio + sideOffset, 1.0f - sideOffset, rowHeight, glm::vec3(1.0f, 0.0f, 0.0f));
 }
 
+void setupGaltonBoard(PS_AGONY::Simulation& simulation)
+{
+    constexpr int ballCount = 1500;
+    constexpr float ballRadius = 0.05f;
+
+    constexpr int pegRows = 20;
+    constexpr int pegColumns = 40;
+    constexpr float pegRadius = 0.04f;
+    constexpr float pegTopY = 0.0f;
+
+    constexpr float binHeight = 5.0f;
+    constexpr float binWidth = 0.01f;
+
+    constexpr float wallThickness = 0.1f;
+    constexpr float wallHeight = binHeight + 8.0f;
+
+    constexpr float funnelOpeningWidth = ballRadius * 4.0f * 1.5f;
+    constexpr float funnelRotation = 0.9f;
+    constexpr float funnelLength = 10.0f;
+    constexpr float funnelWidth = 0.05f;
+    constexpr float funnelYOffset = 2.0f;
+
+    //
+    constexpr float pegSpacing = (ballRadius + 0.03f + pegRadius) * 2.0f;
+    constexpr float pegBottomY = pegTopY - pegRows * pegSpacing - pegRadius;
+    constexpr float pegXBoundary = float(pegColumns - 1) * 0.5f * pegSpacing + pegRadius;
+
+
+    PS_AGONY::Material material0 = {
+        .elasticity = 0.8,
+        .staticFriction = 0.0,
+        .dynamicFriction = 0.0
+    };
+
+    PS_AGONY::MaterialIndex material0Index = simulation.createMaterial(material0);
+
+    // Pegs.
+    for (int y = 0; y < pegRows; y++)
+    {
+        const float pegY = pegTopY - pegRadius - y * pegSpacing;
+
+        const int columns = (y & 1) == 0 ? pegColumns : pegColumns - 1;
+        const float offset = (y & 1) == 0 ? 0.0f : pegSpacing * 0.5f;
+        for (int x = 0; x < columns; x++)
+        {
+            const float pegX = -pegXBoundary + pegRadius + pegSpacing * x + offset;
+
+            simulation.createCircle({ pegX, pegY }, { 0.0f, 0.0f }, 0.0f, 0.0f, 0.0f, material0Index, pegRadius);
+        }
+    }
+
+    // Walls.
+    {
+        constexpr float wallInnerBoundaryX = pegXBoundary + pegSpacing - pegRadius;
+        constexpr float wallInnerBottomY = pegBottomY - binHeight;
+
+        simulation.createBox(
+            { 0.0f, wallInnerBottomY - wallThickness * 0.5f },
+            { 0.0f, 0.0f }, 0.0f, 0.0f, 0.0f,
+            material0Index,
+            { wallInnerBoundaryX * 2.0f, wallThickness}
+        );
+
+        simulation.createBox(
+            { -(wallInnerBoundaryX + wallThickness * 0.5f), wallInnerBottomY + wallHeight * 0.5f },
+            { 0.0f, 0.0f }, 0.0f, 0.0f, 0.0f,
+            material0Index,
+            { wallThickness, wallHeight }
+        );
+
+        simulation.createBox(
+            { (wallInnerBoundaryX + wallThickness * 0.5f), wallInnerBottomY + wallHeight * 0.5f },
+            { 0.0f, 0.0f }, 0.0f, 0.0f, 0.0f,
+            material0Index,
+            { wallThickness, wallHeight }
+        );
+    }
+
+    // Bins.
+    {
+        for (int x = 0; x < pegColumns; x++)
+        {
+            const float binX = -pegXBoundary + pegRadius + pegSpacing * x;
+
+            simulation.createBox(
+                { binX, pegBottomY - binHeight * 0.5f },
+                { 0.0f, 0.0f }, 0.0f, 0.0f, 0.0f,
+                material0Index,
+                { binWidth, binHeight }
+            );
+        }
+    }
+
+    // Funnel.
+    {
+        const float funnelX = funnelOpeningWidth * 0.5f + std::sin(funnelRotation) * funnelLength * 0.5f;
+        const float funnelY = funnelYOffset + std::cos(funnelRotation) * funnelLength * 0.5f;
+
+        simulation.createBox(
+            { -funnelX, funnelY },
+            { 0.0f, 0.0f }, funnelRotation, 0.0f, 0.0f,
+            material0Index,
+            { funnelWidth, funnelLength }
+        );
+
+        simulation.createBox(
+            { funnelX, funnelY },
+            { 0.0f, 0.0f }, -funnelRotation, 0.0f, 0.0f,
+            material0Index,
+            { funnelWidth, funnelLength }
+        );
+    }
+
+    // Balls.
+    {
+        constexpr float spawnBottom = 0.1f + ballRadius + funnelYOffset;
+
+        const float rotTan = std::tan(funnelRotation);
+        const float rndStart = ballRadius * 5.0f;
+        const float rndEnd = std::cos(funnelRotation) * funnelLength - ballRadius;
+
+        for (int i = 0; i < ballCount; i++)
+        {
+            float rnd = Random::real<float>(0.0f, 1.0f);
+            rnd = 1.0f - rnd;
+            rnd *= rnd;
+            rnd = 1.0f - rnd;
+            rnd = rndStart + rnd * (rndEnd - rndStart);
+
+            const float xBorder = funnelOpeningWidth * 0.5f + rnd * rotTan;
+
+            const float x = Random::real<float>(-xBorder, xBorder);
+            const float y = spawnBottom + rnd;
+            simulation.createCircle({ x, y }, { 0.0f, 0.0f }, 0.0f, 0.0f, 1.0f, material0Index, ballRadius);
+        }
+    }
+}
+
 static int gameFunc()
 {
     // Window.
@@ -203,7 +341,7 @@ static int gameFunc()
 
         Random::setSeed(0);
 
-        if constexpr (true)
+        if constexpr (false)
         {
             constexpr float boundary = 12.0f;
             constexpr float thickness = 20.0f;
@@ -253,7 +391,7 @@ static int gameFunc()
                 simulation.createCircle({ 0.0f, boundary + thickness + radius + 0.5f }, { 0.0f, 0.0f }, 0.0f, 1.0f, 20000.0f, material1Index, radius);
             }
         }
-        else
+        else if constexpr (false)
         {
             constexpr int objectCount = 500;
             constexpr float boxSize = 0.1f;
@@ -275,6 +413,10 @@ static int gameFunc()
                 simulation.createBox({ x, boxSize * 0.5f }, { 0.0f, 0.0f }, 0.0f, 0.0f, 1.0f, material0Index, { boxSize, boxSize });
                 simulation.createCircle({ x, boxSize * 0.5f + paddingY }, { 0.0f, 0.0f }, 0.0f, 0.0f, 1.0f, material0Index, boxSize * 0.5f);
             }
+        }
+        else
+        {
+            setupGaltonBoard(simulation);
         }
     }
 
