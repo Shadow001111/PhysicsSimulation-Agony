4d0441cc17024b075faba0718e5ce08e7ee37c77
ShadowBlader <sutuginaleksandr1@gmail.com>
2026-07-06 16:55:38 +0300

    Cosmetic changes.

--- PhysicsSimulation-Agony/Physics/Solver.cpp
+++ PhysicsSimulation-Agony/Physics/Solver.cpp
@@ -105,7 +105,7 @@ namespace PS_AGONY
 
             const Real elasticityPlusOne = (materialA->elasticity + materialB->elasticity) * Real(0.5) + Real(1.0); // Hoping for fused multiply-add. Adding here instead of adding in impulse calculation.
 
-            const Real staticFriction = std::sqrt(std::fmax(Real(0), materialA->staticFriction * materialB->staticFriction));
+            const Real staticFriction  = std::sqrt(std::fmax(Real(0), materialA->staticFriction  * materialB->staticFriction));
             const Real dynamicFriction = std::sqrt(std::fmax(Real(0), materialA->dynamicFriction * materialB->dynamicFriction));
 
             // Compute world centers of mass.
@@ -131,7 +131,7 @@ namespace PS_AGONY
             std::array<Real, 2> jnArray{};
             const uint32_t contactCount = data.contactCount; // std::min(data.contactCount, 2u);
 
-            const Real impulseScale = Real(1.0) / Real(data.contactCount);
+            const Real impulseScale = Real(1.0) / Real(contactCount);
             {
                 bool noContacts = true;
 
